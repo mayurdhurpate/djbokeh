2a9f68bff98d92300479f6eaaddc1f485255076f
Mayur Dhurpate <mayurdhurpate@gmail.com>
2018-06-07 02:08:13 +0530

    looking foor ntcdashboard

--- test1/views.py
+++ test1/views.py
@@ -13,6 +13,6 @@ def index(request):
     script = server_document("http://localhost:5006/linear_example")
     script = server_document("http://localhost:5006/bokeh_dashboard")
     script = server_document("https://bokehdash.herokuapp.com/linear_example")
-    
+    script = server_document("https://ntcdashboard.herokuapp.com/bokeh_dashboard")
     print(bokeh.__version__)
     return render(request,'index.html',{'script':script})
