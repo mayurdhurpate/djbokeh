701cfad2723cd2156e64d86e77b05194e2282198
Mayur Dhurpate <mayurdhurpate@gmail.com>
2018-06-06 18:00:34 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djbokeh.wsgi --log-file -
---
+++ test1/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bokeh.embed import server_document
+import bokeh
+
+# Create your views here.
+
+
+
+
+def index(request):
+    script = server_document("https://bokehdash.herokuapp.com")
+    script = server_document("http://localhost:5006/linear_example")
+    script = server_document("http://localhost:5006/bokeh_dashboard")
+    
+    print(bokeh.__version__)
+    return render(request,'index.html',{'script':script})
